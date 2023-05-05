d1e59a35370a48f078fedcd2994c465a615770b3
Irine7 <irine7@yandex.ru>
2023-05-05 20:10:38 +0200

    fix: first task change

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,7 +6,8 @@ const path = require('path');
 const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
 let data = '';
+
 // Собираем все чанки вместе
 stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log('End', data));
+stream.on('end', () => console.log(data));
 stream.on('error', error => console.error('Error:', error));
