5116c0232b28d9558ab23817074bef9b8996c11a
Irine7 <irine7@yandex.ru>
2023-05-05 16:57:58 +0200

    feat: add first task solve

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+// Для подключения модуля используем функцию require()
+const fs = require('fs');
+const path = require('path');
+
+// Получаем стрим для чтения файлов
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let data = '';
+// Собираем все чанки вместе
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log('End', data));
+stream.on('error', error => console.error('Error:', error));
