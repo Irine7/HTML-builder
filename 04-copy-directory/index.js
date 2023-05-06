c0ca15e822aabfccee8e2e171a5211e115cd93fa
Irine7 <irine7@yandex.ru>
2023-05-06 18:23:37 +0200

    feat: add fourth task solve

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+// Создаем папку files-copy, если ее не существует
+async function copyFolder() {
+	const sourceFolder = path.resolve(__dirname, 'files');
+	const newFolder = path.resolve(__dirname, 'files-copy');
+
+	try {
+		await fs.mkdir(newFolder, { recursive: true });
+	} catch (error) {
+		throw error;
+	}
+
+	// Читаем содержимое папки files
+	const files = await fs.readdir(sourceFolder);
+
+	// Копируем файлы из папки files в папку files-copy
+	await Promise.all(
+		files.map(async (file) => {
+			const sourcePath = path.join(sourceFolder, file);
+			const newPath = path.join(newFolder, file);
+
+			const stat = await fs.stat(sourcePath);
+			if (stat.isFile()) {
+				await fs.copyFile(sourcePath, newPath);
+			}
+		})
+	);
+	
+	console.log('End of copying');
+}
+
+copyFolder().catch((error) => {
+	console.error('Error ocurred:', error);
+});
