a9feeacc27d1fd5d5740c505040a15e09cc6cf8a
Irine7 <irine7@yandex.ru>
2023-05-09 00:09:20 +0200

    fix: sixth task change (add assets fixed)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,30 +9,24 @@ let outputFolderPath = path.join(__dirname, 'project-dist');
 
 async function copyFolder(sourceFolderPath, targetFolderPath) {
 	try {
-	// Создаем целевую папку (project-dist)
-	await fs.promises.mkdir(targetFolderPath);
+		await fs.promises.mkdir(targetFolderPath);
+		const files = await fs.promises.readdir(sourceFolderPath);
 
-	// Читаем папки и файлы в исходной папке
-	const files = await fs.promises.readdir(sourceFolderPath);
+		for (const file of files) {
+			const sourcePath = path.join(sourceFolderPath, file);
+			const targetPath = path.join(targetFolderPath, file);
+			const fileStat = await fs.promises.stat(sourcePath);
 
-	// Рекурсивно копируем файлы и папки
-	for (const file of files) {
-		const sourcePath = path.join(sourceFolderPath, file);
-		const targetPath = path.join(targetFolderPath, file);
-		const fileStat = await fs.promises.stat(sourcePath);
-
-		if (fileStat.isFile()) {
-			// Копируем файлы
-			await fs.promises.copyFile(sourcePath, targetPath);
-		} else if (fileStat.isDirectory()) {
-			// Рекурсивно копируем папку
-			await copyFolder(sourcePath, targetPath);
+			if (fileStat.isFile()) {
+				await fs.promises.copyFile(sourcePath, targetPath);
+			} else if (fileStat.isDirectory()) {
+				await copyFolder(sourcePath, targetPath);
+			}
 		}
+		console.log(`Copied folder from ${sourceFolderPath} to ${targetFolderPath} successfully!`);
+	} catch (error) {
+		console.error(`Error copying folder from ${sourceFolderPath} to ${targetFolderPath}:`, error);
 	}
-	console.log(`copied folder from ${sourceFolderPath} to ${targetFolderPath} successfully!`);
-} catch (error) {
-	console.error(`error copying folder from ${sourceFolderPath} to ${targetFolderPath}:`, error);
-}
 }
 
 // Читаем шаблонный файл
@@ -41,7 +35,7 @@ async function readTemplateFile() {
 		const templateContent = await fs.promises.readFile(templateFilePath, 'utf8');
 		return templateContent;
 	} catch (error) {
-		console.error('error reading template file:', error);
+		console.error('Error reading template file:', error);
 		throw error;
 	}
 }
@@ -60,18 +54,18 @@ function findComponentTags(templateContent) {
 
 // Заменяем теги модифицированным контентом
 async function replaceComponentTags(templateContent, tags) {
-	let modifiedContent = templateContent;
-	for (const tag of tags) {
-		const componentFilePath = path.join(componentsFolderPath, `${tag}.html`);
-		try {
-			const componentContent = await fs.promises.readFile(componentFilePath, 'utf8');
-			modifiedContent = modifiedContent.replace(new RegExp(`{{\\s*${tag}\\s*}}`, 'g'), componentContent);
-		} catch (error) {
-			console.error(`error reading (${tag}.html):`, error);
-			throw error;
-		}
+let modifiedContent = templateContent;
+for (const tag of tags) {
+	const componentFilePath = path.join(componentsFolderPath, `${tag}.html`);
+	try {
+		const componentContent = await fs.promises.readFile(componentFilePath, 'utf8');
+		modifiedContent = modifiedContent.replace(new RegExp(`{{\\s*${tag}\\s*}}`, 'g'), componentContent);
+	} catch (error) {
+		console.error(`Error reading component file (${tag}.html):`, error);
+		throw error;
 	}
-	return modifiedContent;
+}
+return modifiedContent;
 }
 
 // Создаем index.html
@@ -79,9 +73,9 @@ async function createIndexHtml(content) {
 	const outputPath = path.join(outputFolderPath, 'index.html');
 	try {
 		await fs.promises.writeFile(outputPath, content, 'utf8');
-		console.log('index.html was created');
+		console.log('index.html created successfully!');
 	} catch (error) {
-		console.error('error creating index.html file:', error);
+		console.error('Error creating index.html:', error);
 		throw error;
 	}
 }
@@ -96,7 +90,7 @@ async function createCssBundle() {
 				const fileContent = await fs.promises.readFile(filePath, 'utf8');
 				return fileContent;
 			} catch (error) {
-				console.error(`error reading css file (${cssFile}):`, error);
+				console.error(`Error reading CSS file (${cssFile}):`, error);
 				throw error;
 			}
 		})
@@ -104,124 +98,120 @@ async function createCssBundle() {
 	const bundlePath = path.join(outputFolderPath, 'style.css');
 	try {
 		await fs.promises.writeFile(bundlePath, cssContent.join('\n'), 'utf8');
-		console.log('style.css was created');
+		console.log('style.css created successfully!');
 	} catch (error) {
-		console.error('error creating style.css file:', error);
+		console.error('Error creating style.css:', error);
 		throw error;
 	}
 }
 
 // Копируем папку assets
-async function copyAssetsFolder() {
-	const assetsOutputFolderPath = path.join(outputFolderPath, 'assets');
-		try {
-			await fs.promises.mkdir(assetsOutputFolderPath);
-			console.log('assets was folder created');
-		} catch (error) {
-		if (error.code !== 'EEXIST') {
-			console.error('error creating assets folder:', error);
+async function copyFolder(sourceFolderPath, targetFolderPath) {
+	try {
+		await fs.promises.mkdir(targetFolderPath);
+
+		const files = await fs.promises.readdir(sourceFolderPath);
+
+		for (const file of files) {
+			const sourcePath = path.join(sourceFolderPath, file);
+			const targetPath = path.join(targetFolderPath, file);
+
+			const fileStat = await fs.promises.stat(sourcePath);
+
+			if (fileStat.isFile()) {
+				await fs.promises.copyFile(sourcePath, targetPath);
+				console.log(`Copied ${file} to assets folder`);
+			} else if (fileStat.isDirectory()) {
+				await copyFolder(sourcePath, targetPath);
+			}
+		}
+		console.log(`Copied folder from ${sourceFolderPath} to ${targetFolderPath} successfully!`);
+	} catch (error) {
+		console.error(`Error copying folder from ${sourceFolderPath} to ${targetFolderPath}:`, error);
 		throw error;
+	}
+}
+
+
+async function copyAssetsFolder() {
+	const srcFolder = path.join(__dirname, 'assets');
+	const destFolder = path.join(outputFolderPath, 'assets');
+
+	try {
+		await fs.promises.access(destFolder);
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			// Создать папку, если она не существует
+			await fs.promises.mkdir(destFolder, { recursive: true });
+		} else {
+			throw error; // Пробросить ошибку, если возникла другая ошибка
 		}
 	}
 
-	const files = await fs.promises.readdir(assetsFolderPath);
-	for (const file of files) {
-		const srcPath = path.join(assetsFolderPath, file);
-		const destPath = path.join(assetsOutputFolderPath, file);
-		try {
+	try {
+		await copyFilesRecursively(srcFolder, destFolder);
+		console.log('Assets folder copied successfully');
+	} catch (error) {
+		console.error('Error copying assets folder:', error);
+	}
+}
+
+async function copyFilesRecursively(src, dest) {
+	const entries = await fs.promises.readdir(src, { withFileTypes: true });
+
+	for (const entry of entries) {
+		const srcPath = path.join(src, entry.name);
+		const destPath = path.join(dest, entry.name);
+
+		if (entry.isDirectory()) {
+			await copyFilesRecursively(srcPath, destPath);
+		} else {
 			await fs.promises.copyFile(srcPath, destPath);
-			console.log(`copied ${file} to assets folder`);
-		} catch (error) {
-			console.error(`error copying ${file} to assets folder:`, error);
-			throw error;
 		}
 	}
-	console.log('assets folder was copied');
 }
 
 // Создаем новую страницу
 async function buildPage() {
 	try {
-	// Проверяеv существetn ли папка project-dist
-	const exists = await fs.promises.access(outputFolderPath)
-	.then(() => true)
-	.catch(() => false);
+		// Проверяем существует ли папка project-dist
+		const exists = await fs.promises.access(outputFolderPath)
+			.then(() => true)
+			.catch(() => false);
 
-	// Создаем папку project-dist если ее нет
-	if (!exists) {
-		await fs.promises.mkdir(outputFolderPath);
-	}
-
-	// Читаем и сохраняем шаблон
-	let templateContent = await fs.promises.readFile(templateFilePath, 'utf-8');
-
-	// Находим все теги в шаблоне
-	const tagNames = templateContent.match(/{{(.*?)}}/g).map((tag) => tag.slice(2, -2));
-
-	// Замяем шаблонные теги из содержимого папки components
-	for (const tagName of tagNames) {
-		const componentFilePath = path.join(componentsFolderPath, `${tagName}.html`);
-		const componentContent = await fs.promises.readFile(componentFilePath, 'utf-8');
-		templateContent = templateContent.replace(`{{${tagName}}}`, componentContent);
-	}
-
-	// Записываем измененный шаблон в index.html в папке project-dist
-	const outputPath = path.resolve(outputFolderPath, 'index.html');
-	await fs.promises.writeFile(outputPath, templateContent, 'utf-8');
-	console.log('index.html was created');
-
-	// Собираем все стили в один файл
-	const stylesFiles = await fs.promises.readdir(stylesFolderPath);
-	let stylesContent = '';
-	for (const file of stylesFiles) {
-		const filePath = path.join(stylesFolderPath, file);
-		const fileContent = await fs.promises.readFile(filePath, 'utf-8');
-		stylesContent += fileContent;
-	}
-
-	// Записываем стили в style.css в папке project-dist
-	const stylesOutputPath = path.join(outputFolderPath, 'style.css');
-	await fs.promises.writeFile(stylesOutputPath, stylesContent, 'utf-8');
-	console.log('style.css file was created');
-
-	// Создаем папку assets если ее нет
-	const assetsOutputFolderPath = path.join(outputFolderPath, 'assets');
-	const assetsExists = await fs.promises.access(assetsOutputFolderPath)
-		.then(() => true)
-		.catch(() => false);
-
-	if (!assetsExists) {
-		await fs.promises.mkdir(assetsOutputFolderPath);
-	}
-
-	// Копируем папку assets в project-dist/assets
-	const assetsFiles = await fs.promises.readdir(assetsFolderPath);
-	for (const file of assetsFiles) {
-		const srcPath = path.join(assetsFolderPath, file);
-		const destPath = path.join(assetsOutputFolderPath, file);
-		const stats = await fs.promises.stat(srcPath);
-		if (stats.isFile()) {
-			await fs.promises.copyFile(srcPath, destPath);
-			console.log(`Copied ${file} to the assets folder`);
+		// Создаем папку project-dist если ее нет
+		if (!exists) {
+			await fs.promises.mkdir(outputFolderPath);
 		}
-	}
-	console.log('Page was built');
+
+		// Читаем и сохраняем шаблон
+		let templateContent = await fs.promises.readFile(templateFilePath, 'utf-8');
+
+		// Находим все теги в шаблоне
+		const tagNames = templateContent.match(/{{(.*?)}}/g).map((tag) => tag.slice(2, -2));
+
+		// Заменяем шаблонные теги из содержимого папки components
+		for (const tagName of tagNames) {
+			const componentFilePath = path.join(componentsFolderPath, `${tagName}.html`);
+			const componentContent = await fs.promises.readFile(componentFilePath, 'utf-8');
+			templateContent = templateContent.replace(`{{${tagName}}}`, componentContent);
+		}
+
+		// Записываем измененный шаблон в index.html в папке project-dist
+		const outputPath = path.resolve(outputFolderPath, 'index.html');
+		await fs.promises.writeFile(outputPath, templateContent, 'utf-8');
+		console.log('Index.html created successfully!');
+
+		// Собираем все стили в один файл
+		await createCssBundle();
+
+		// Создаем папку assets если ее нет и копируем содержимое
+		await copyAssetsFolder();
+
+		console.log('Page built successfully!');
 	} catch (error) {
-	console.error('Error building the page:', error);
+		console.error('Error building the page:', error);
 	}
 }
 
 buildPage();
-
-// Проверяем существует ли папка
-async function checkFolderExists(folderPath) {
-	try {
-		const stats = await fs.promises.stat(folderPath);
-		return stats.isDirectory();
-	} catch (error) {
-	if (error.code === 'ENOENT') {
-		return false;
-	}
-	throw error;
-	}
-}
